f494e8855c2e69e67609176813f039af8ea1dd99
neilha <neilharrold@gmail.com>
2026-07-31 14:47:03 +0100

    resets Capybara sessions after each request and system spec

--- spec/support/authentication.rb
+++ spec/support/authentication.rb
@@ -2,6 +2,7 @@ RSpec.configure do |config|
   %i[request system].each do |spec_type|
     config.after(:each, type: spec_type) do
       PublishingPlatform::SSO.test_user = nil
+      Capybara.reset_sessions!
     end
   end
 end
